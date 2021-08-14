fe84c72f7494e4e9f2f198a77c509b9cae6a599f
Kai Tietz <ktietz@anaconda.com>
2021-08-14 19:30:09 +0200

    add support for osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,9 @@
 
 cd $SRC_DIR
 
+# Get an updated config.sub and config.guess
+cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* .
+
 if [ "$(uname)" == "Darwin" ]
 then
     export CXX="${CXX} -stdlib=libc++"
@@ -9,6 +12,9 @@ then
 fi
 
 autoreconf -f -i
+
+# Get an updated config.sub and config.guess
+cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* .
 
 ./configure --prefix=$PREFIX --disable-silent-rules --disable-dependency-tracking
 make
